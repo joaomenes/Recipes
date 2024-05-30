1641e4ed7d3680ea832555c6bc07368bbaa63a3a
Joao Menes <joao@norton.me>
2024-05-30 17:09:53 -0300

    Corrigindo a busca

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,12 @@
-from django.http import Http404
-from django.shortcuts import get_list_or_404, render, get_object_or_404
+from django.db.models import Q
+from django.http.response import Http404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from recipes.models import Recipe
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
     ).order_by('-id')
-    
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -18,7 +18,6 @@ def category(request, category_id):
             is_published=True,
         ).order_by('-id')
     )
-
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
         'title': f'{recipes[0].category.name} - Category | '
@@ -26,16 +25,23 @@ def category(request, category_id):
 
 def recipe(request, id):
     recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
-
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
     })
 
 def search(request):
-    search_term = request.GET.get('q')
-
+    search_term = request.GET.get('q', '').strip()
     if not search_term:
         raise Http404()
-    
-    return render(request, 'recipes/pages/search.html')
+
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) |
+        Q(description__icontains=search_term),
+    ).order_by('-id')
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': recipes,
+    })
